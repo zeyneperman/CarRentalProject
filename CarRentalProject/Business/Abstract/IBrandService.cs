cb6dc825a7bfbd25c3526e9b4e39d0997618b57c
zeyneperman <zeynepzpn25@gmail.com>
2021-04-06 21:51:57 +0300

    düzenleme

--- CarRentalProject/Business/Abstract/IBrandService.cs
+++ CarRentalProject/Business/Abstract/IBrandService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace Business.Abstract
 {
     public interface IBrandService
-    {
+    { 
         IDataResult<List<Brand>> GetAll();
         IResult Add(Brand brand);
         IDataResult<Brand> GetById(int brandId);
